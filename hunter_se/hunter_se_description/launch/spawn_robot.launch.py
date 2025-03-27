ff754cb7840105c9d3ccb1259ad0e95400d5f5d8
Jacopo Cioni <cionix90@gmail.com>
2025-03-27 17:51:27 +0100

    add lidar and imu sensor

--- hunter_se/hunter_se_description/launch/spawn_robot.launch.py
+++ hunter_se/hunter_se_description/launch/spawn_robot.launch.py
@@ -12,25 +12,72 @@ from launch import LaunchDescription, LaunchContext
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
-def create_yaml(context: LaunchContext, robot_name):
+def create_yaml(context: LaunchContext, robot_name,joystic_teleop):
     robot_name_str = context.perform_substitution(robot_name)
-    # if(robot_name_str == ""):
-    #     robot_name_str = "alpha"
-    path = os.path.join(get_package_share_path("hunter_se_description"),"config", "sim_controllers.yaml")
-    with open(path) as stream:
-        param_str = stream.read()
-        print(type(param_str))
-        subs_par = param_str.replace("tf_prefix",robot_name_str)
-    
-    file = yaml.safe_load(subs_par)
-    file_name = path = os.path.join(get_package_share_path("hunter_se_description"),"config", f"{robot_name_str}_controllers.yaml")
-    with open(file_name,"w") as stream:
-        yaml.dump(file, stream, default_flow_style=False)
+    if(robot_name_str != ""):  
+        path = os.path.join(get_package_share_path("hunter_se_description"),"config", "sim_controllers.yaml")
+        with open(path) as stream:
+            param_str = stream.read()
+            print(type(param_str))
+            subs_par = param_str.replace("tf_prefix",robot_name_str)
+        
+        file = yaml.safe_load(subs_par)
+        file_name = path = os.path.join(get_package_share_path("hunter_se_description"),"config", f"{robot_name_str}_controllers.yaml")
+        with open(file_name,"w") as stream:
+            yaml.dump(file, stream, default_flow_style=False)
      
     # new_file = {robot_name: file[] }
 
 
-    print(f'robot name:: {robot_name_str}')
+        teleop_node = Node(
+            package="teleop_twist_joy",
+            executable="teleop_node",
+            condition=IfCondition(joystic_teleop),
+            parameters=[os.path.join(get_package_share_path("hunter_se_description"),"config", "teleop_conf.yaml")],
+            remappings=[
+                    ('/cmd_vel', "/"+ robot_name_str+"/ackerman_control/reference")]
+        )
+        bridge = Node(
+            package="ros_gz_bridge",
+            executable="parameter_bridge",
+            arguments=[
+                [robot_name_str+"/", "imu@sensor_msgs/msg/Imu[ignition.msgs.IMU"],
+                # [robot_name,"gt_odom@nav_msgs/msg/Odometry@ignition.msgs.Odometry"],
+                "/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock",
+                [robot_name_str+"/","rgl_lidar@sensor_msgs/msg/PointCloud2@ignition.msgs.PointCloudPacked"],
+                # "/rgbd_camera/image@sensor_msgs/msg/Image@ignition.msgs.Image",
+                # "/rgbd_camera/camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo",
+                # "/rgbd_camera/depth_image@sensor_msgs/msg/Image@ignition.msgs.Image",
+                # "/rgbd_camera/points@sensor_msgs/msg/PointCloud2@ignition.msgs.PointCloudPacked"
+            ],
+            output="screen",
+            )
+    else:
+        teleop_node = Node(
+            package="teleop_twist_joy",
+            executable="teleop_node",
+            condition=IfCondition(joystic_teleop),
+            parameters=[os.path.join(get_package_share_path("hunter_se_description"),"config", "teleop_conf.yaml")],
+            remappings=[
+                    ('/cmd_vel', "/ackerman_control/reference")]
+        )
+        bridge = Node(
+            package="ros_gz_bridge",
+            executable="parameter_bridge",
+            arguments=[
+                ["imu@sensor_msgs/msg/Imu[ignition.msgs.IMU"],
+                # [robot_name,"gt_odom@nav_msgs/msg/Odometry@ignition.msgs.Odometry"],
+                "/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock",
+                ["rgl_lidar@sensor_msgs/msg/PointCloud2@ignition.msgs.PointCloudPacked"],
+                # "/rgbd_camera/image@sensor_msgs/msg/Image@ignition.msgs.Image",
+                # "/rgbd_camera/camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo",
+                # "/rgbd_camera/depth_image@sensor_msgs/msg/Image@ignition.msgs.Image",
+                # "/rgbd_camera/points@sensor_msgs/msg/PointCloud2@ignition.msgs.PointCloudPacked"
+            ],
+            output="screen",
+            )
+    return [teleop_node,bridge]
+
 
 def generate_launch_description():
 
@@ -41,6 +88,8 @@ def generate_launch_description():
     spawn_pos_x = LaunchConfiguration("spawn_pos_x")
     spawn_pos_y = LaunchConfiguration("spawn_pos_y")
     spawn_pos_z = LaunchConfiguration("spawn_pos_z")
+    joystic_teleop = LaunchConfiguration("joystic_teleop")
+
     hunter_robot_path = get_package_share_path("hunter_se_description")
     hunter_robot_path = os.path.join(hunter_robot_path,"urdf", "hunter_se_alone.xacro")
 
@@ -56,27 +105,36 @@ def generate_launch_description():
 
     robot_name_arg = DeclareLaunchArgument(
                 name="robot_name",
-                default_value="alpha",
+                default_value="",
                 description="Robot Name, Usefull to spawn different robots in the same simulation",
             )
     spawn_pos_x_arg = DeclareLaunchArgument(
             name="spawn_pos_x",
-            default_value="0.0",
+            default_value="-4.5",
             description="spawn x position",
         )
     spawn_pos_y_arg = DeclareLaunchArgument(
             name="spawn_pos_y",
-            default_value="0.0",
+            default_value="-30.0",
             description="spawn y position",
         )
     spawn_pos_z_arg = DeclareLaunchArgument(
             name="spawn_pos_z",
-            default_value="0.5",
+            default_value="8.5",
             description="spawn z position",
+        )
+    joystic_teleop_arg = DeclareLaunchArgument(
+            name="joystic_teleop",
+            default_value="true",
+            description="use joystic to teleoperate the robot ",
         )
     ld.add_action(hunter_model)
     ld.add_action(log_lev_arg)
     ld.add_action(robot_name_arg)
+    ld.add_action(spawn_pos_x_arg)
+    ld.add_action(spawn_pos_y_arg)
+    ld.add_action(spawn_pos_z_arg)
+    ld.add_action(joystic_teleop_arg)
 
     #use command to create a parameter with urdf of mulinex by xacro file
     robot_description = ParameterValue(
@@ -104,9 +162,11 @@ def generate_launch_description():
             "-topic",
             "robot_description",
             "-z",
-            "0.5",
+            spawn_pos_z,
             "-x",
-            "-2.0",
+            spawn_pos_x,
+            "-y",
+            spawn_pos_y,
             "--ros-args",
             "--log-level",
             log_level,
@@ -116,22 +176,23 @@ def generate_launch_description():
     ld.add_action(spawn_entity)
 
     ld.add_action(OpaqueFunction(
-        function=create_yaml, args=[robot_name]
+        function=create_yaml, args=[robot_name,joystic_teleop]
     ))
 
     joy_node = Node(
         package="joy",
         executable="joy_node",
+        condition=IfCondition(joystic_teleop)
     )
 
     ld.add_action(joy_node)
-
-    teleop_node = Node(
-        package="teleop_twist_joy",
-        executable="teleop_node",
-        parameters=[os.path.join(get_package_share_path("hunter_se_description"),"config", "teleop_conf.yaml")],
-        remappings=[
-                ('/cmd_vel', '/alpha/ackerman_control/reference')]
-    )
-    ld.add_action(teleop_node)
+    # teleop_node = Node(
+    #     package="teleop_twist_joy",
+    #     executable="teleop_node",
+    #     condition=IfCondition(joystic_teleop),
+    #     parameters=[os.path.join(get_package_share_path("hunter_se_description"),"config", "teleop_conf.yaml")],
+    #     remappings=[
+    #             ('/cmd_vel', "/"+ +"/ackerman_control/reference")]
+    # )
+    # ld.add_action(teleop_node)
     return ld
